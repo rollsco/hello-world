d8a431fa86449035ce401a4d2a4ac2bf810557e0
Juan M <juan.martinez@districtm.net>
2019-09-29 16:31:48 -0400

    RC-19 Update for real Rolls products

--- src/components/Product/Content.js
+++ src/components/Product/Content.js
@@ -1,31 +1,15 @@
 import React from "react";
-import styled from "styled-components";
-import { CardHeader, CardContent, IconButton } from "@material-ui/core";
+import { CardContent, Typography } from "@material-ui/core";
 import { currency } from "../../services/formatter";
 
-const StyledCardContent = styled(CardContent)`
-  && {
-    padding: 0;
-  }
-`;
-
-const StyledCardHeader = styled(CardHeader)`
-  text-transform: capitalize;
-`;
-
-const ProductPhoto = styled.img`
-  margin: 8px 0;
-  width: 100%;
-`;
-
 const Content = ({ product }) => (
-  <StyledCardContent>
-    <StyledCardHeader
-      title={product.name}
-      subheader={currency(product.price)}
-    />
-    <ProductPhoto src={`https://source.unsplash.com/${product.img}/300x150`} />
-  </StyledCardContent>
+  <CardContent>
+    <Typography variant="subtitle1">{product.name}</Typography>
+    <Typography variant="subtitle2" color="secondary">
+      {currency(product.price)}
+    </Typography>
+    <Typography variant="caption">{product.pieces} piezas</Typography>
+  </CardContent>
 );
 
 export default Content;
--- src/services/formatter.js
+++ src/services/formatter.js
@@ -1,7 +1,12 @@
+import React from "react";
+
 export const currency = value =>
   new Intl.NumberFormat("es-CO", {
     style: "currency",
     currency: "COP",
     minimumFractionDigits: 0,
-    maximumFractionDigits: 0
+    maximumFractionDigits: 0,
   }).format(value);
+
+export const multiline = value =>
+  value.split("\n").map((line, index) => <p key={index}>{line}</p>);
