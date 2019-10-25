268e4d3f873d536ccfe8f7d0c635d0edc213aded
juanmartinzzz <juan.martinzzz@gmail.com>
2019-10-24 22:44:07 -0400

    RC-36 Update Delivery information, make texts smaller (#20)

--- src/components/Cart/DeliveryNotices/DeliveryNotice.js
+++ src/components/Cart/DeliveryNotices/DeliveryNotice.js
@@ -14,10 +14,10 @@ import { currency } from "../../../services/formatter";
 const Row = ({ location, minimumPrice, maximumPrice }) => (
   <TableRow>
     <TableCell>
-      <Typography variant="subtitle2">{location}</Typography>
+      <Typography variant="body2">{location}</Typography>
     </TableCell>
     <TableCell align="right">
-      <Typography variant="subtitle2">{`${currency(minimumPrice)} a ${currency(
+      <Typography variant="body2">{`${currency(minimumPrice)} a ${currency(
         maximumPrice,
       )}`}</Typography>
     </TableCell>
@@ -25,14 +25,13 @@ const Row = ({ location, minimumPrice, maximumPrice }) => (
 );
 
 const DeliveryNotice = () => (
-  // <CartPaper>
   <Table size="small">
     <TableHead>
       <TableRow>
         <TableCell colSpan={99}>
           <Toolbar>
             <Moto />
-            <Typography variant="body2">
+            <Typography variant="caption">
               Te cobrarán tu domicilio al momento de la entrega. El costo
               depende de tu ubicación.
             </Typography>
@@ -42,8 +41,9 @@ const DeliveryNotice = () => (
     </TableHead>
     <TableBody>
       <Row location="Bucaramanga" minimumPrice={4000} maximumPrice={6000} />
-      <Row location="Floridablenca" minimumPrice={6000} maximumPrice={8000} />
+      <Row location="Floridablanca" minimumPrice={6000} maximumPrice={8000} />
       <Row location="Girón" minimumPrice={8000} maximumPrice={9000} />
+      <Row location="Piedecuesta" minimumPrice={9000} maximumPrice={16000} />
     </TableBody>
   </Table>
   // </CartPaper>
