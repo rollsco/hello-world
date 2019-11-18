aba195b4c60475d026a75d6079c67596896e3dcc
juanmartinzzz <juan.martinzzz@gmail.com>
2019-11-17 21:54:00 -0500

    RC-55 Close store at correct times and use Firestore timestamp; Add analytics events (#34)

---
+++ src/constants/time.js
@@ -0,0 +1,7 @@
+export const SUNDAY = 0;
+export const MONDAY = 0;
+export const TUESDAY = 0;
+export const WEDNESDAY = 0;
+export const THURSDAY = 0;
+export const FRIDAY = 0;
+export const SATURDAY = 0;
