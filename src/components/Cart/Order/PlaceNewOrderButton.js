f9564f89609f8219c2df06d9054f4f8ca6996fed
Juan M <juan.martinez@districtm.net>
2019-12-23 15:59:42 -0500

    Add step progress and animations to Order status visualization

--- src/components/Cart/Order/PlaceNewOrderButton.js
+++ src/components/Cart/Order/PlaceNewOrderButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "@material-ui/core";
-import { CartButtonBox } from "./components";
+import { CartButtonBox } from "../components";
 
 const PlaceNewOrderButton = ({ order, placeNewOrder }) => {
   if (!order.status) {
