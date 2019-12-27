2f7018eedfd7a926ff62e758ac1971ca87483772
Juan M <juan.martinez@districtm.net>
2019-12-27 10:33:11 -0500

    Remove services directory

--- src/services/deserializer/deserializer.js
+++
@@ -1,9 +0,0 @@
-const JSONAPIDeserializer = require("jsonapi-serializer").Deserializer;
-const deserializerOptions = { keyForAttribute: "camelCase" };
-
-export const jsonApiDeserialize = async response => {
-  const responseData = await response.json();
-  return await new JSONAPIDeserializer(deserializerOptions).deserialize(
-    responseData
-  );
-};
