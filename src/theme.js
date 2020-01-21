3a40e5e2d89fcb436915a78ac0c293712ec90197
Juan M <juan.martinez@districtm.net>
2020-01-21 12:12:15 -0500

    Format

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export const red = "rgb(140,26,0)";
 export const black = "rgb(70,70,70)";
 export const green = "rgb(0,70,10)";
-export const golden = "rgb(249, 168, 37)";
+export const golden = "rgb(249,168,37)";
 
 export default createMuiTheme({
   palette: {
