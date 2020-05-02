2ffeeb96db5bf0b598f76a99551eef090cd2c65d
Juan M <juan.martinez@districtm.net>
2020-05-02 12:10:43 -0400

    Change colours on load

--- src/theme.js
+++ src/theme.js
@@ -5,26 +5,38 @@ export const black = "rgb(70,70,70)";
 export const green = "rgb(0,70,10)";
 export const golden = "rgb(249,168,37)";
 
+const colourCombinations = [
+  { type: "dark", primary: "#212121", secondary: "#f9a825" },
+  { type: "dark", primary: "#440027", secondary: "#CD88AF" },
+  { type: "dark", primary: "#0A4600", secondary: "#96D38D" },
+  { type: "dark", primary: "#555300", secondary: "#FFFDAA" },
+  { type: "dark", primary: "#061639", secondary: "#7788AA" },
+  { type: "dark", primary: "#9D1200", secondary: "#FF6855" },
+  { type: "dark", primary: "#1D0666", secondary: "#D84197" },
+  { type: "light", primary: "#9D1200", secondary: "#FF3F25" },
+  { type: "light", primary: "#9576AB", secondary: "#3A1356" },
+  { type: "light", primary: "#96D38D", secondary: "#1E6912" },
+  { type: "light", primary: "#f9a825", secondary: "#121212" },
+  { type: "light", primary: "#82C38D", secondary: "#804815" },
+  { type: "light", primary: "#FFD3AA", secondary: "#804815" },
+  { type: "light", primary: "#FFD3AA", secondary: "#10621E" },
+  { type: "light", primary: "#1B79B1", secondary: "#AA0036" },
+  { type: "light", primary: "#664AC2", secondary: "#990057" },
+];
+
+const length = colourCombinations.length;
+const current = colourCombinations[Math.floor(Math.random() * length)];
+
 export default createMuiTheme({
   palette: {
-    type: "dark",
+    type: current.type,
     primary: {
-      main: "#fbc02d",
-      main: "#303f9f",
-      main: "#9c27b0",
-      main: "#009688",
-      main: "#2196f3",
-      main: "#673ab7",
       main: "#212121",
+      main: current.primary,
     },
     secondary: {
-      main: "#e64a19",
-      main: "#2196f3",
-      main: "#7b1fa2",
-      main: "#dd2c00",
-      main: "#ab003c",
-      main: "#ff9100",
       main: "#f9a825",
+      main: current.secondary,
     },
   },
   typography: {
